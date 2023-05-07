283a90241df376ff80d404e7d08755176fea103d
firasy23 <131626599+firasy23@users.noreply.github.com>
2023-05-07 17:55:17 -0500

    Add files via upload
    These are the corrected codes that work. each code previously contained minor errors such as syntax etc. The issue is resolved and the code is working

---
+++ pirate.py CORRECTED.py
@@ -0,0 +1,15 @@
+
+#CORRECTED VERSION
+
+greeting = input("Hello, possible pirate! What's the password? ")
+
+if greeting == "Arrr!":
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
+greeting = input("Hello, possible pirate! What's the password? ")
+
+if greeting == "Arrr!":
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
---
+++ time2.py corrected.py
@@ -0,0 +1,17 @@
+
+#CORRECTED VERSION
+
+str_time = input("What time is it now? ")
+str_wait_time = input("What is the number of hours to wait? ")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_goes_off = time + wait_time
+print(time_when_alarm_goes_off)
+str_time = input("What time is it now? ")
+str_wait_time = input("What is the number of hours to wait? ")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_goes_off = time + wait_time
+print(time_when_alarm_goes_off)
